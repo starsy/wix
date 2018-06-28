14720e0440d0e967ad2574b604932680a5df4794
Alex Song <starsy@gmail.com>
2018-06-28 12:04:42 +0800

    match build.bat with latest changes

--- wix/build.bat
+++ wix/build.bat
@@ -1,2 +1,2 @@
-"c:\Program Files (x86)\WiX Toolset v3.11\bin"\candle Product.wxs
-"c:\Program Files (x86)\WiX Toolset v3.11\bin"\light -ext WixUIExtension Product.wixobj
+"c:\Program Files (x86)\WiX Toolset v3.11\bin"\candle -ext WixUtilExtension Product.wxs 
+"c:\Program Files (x86)\WiX Toolset v3.11\bin"\light -ext WixUIExtension -ext WixUtilExtension Product.wixobj
