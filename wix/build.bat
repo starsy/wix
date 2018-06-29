c820d6b7eb511e6044494763de9686dc45e3d60a
Alex Song <starsy@gmail.com>
2018-06-29 08:44:41 +0800

    fix eula file issue

--- wix/build.bat
+++ wix/build.bat
@@ -1,3 +1,2 @@
 "c:\Program Files (x86)\WiX Toolset v3.11\bin"\candle -ext WixUtilExtension Product.wxs 
-rem "c:\Program Files (x86)\WiX Toolset v3.11\bin"\light -ext WixUIExtension -ext WixUtilExtension Product.wixobj
-"c:\Program Files (x86)\WiX Toolset v3.11\bin"\light -ext WixUIExtension -ext WixUtilExtension -cultures:zh-cn -loc zh-cn.wxl Product.wixobj 
+"c:\Program Files (x86)\WiX Toolset v3.11\bin"\light -ext WixUIExtension -ext WixUtilExtension -cultures:zh-cn -loc zh-cn.wxl -dWixUILicenseRtf=eula_zh-cn.rtf Product.wixobj 
